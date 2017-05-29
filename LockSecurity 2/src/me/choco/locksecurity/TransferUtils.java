8b692e1d2cf0966ddfa04deed99c16c9e215268d
2008Choco <hawkeboyz2@hotmail.com>
2017-05-29 09:07:22 -0400

    FINALLY added a method body for TransferUtils#fromFile()

--- LockSecurity 2/src/me/choco/locksecurity/TransferUtils.java
+++ LockSecurity 2/src/me/choco/locksecurity/TransferUtils.java
@@ -1,5 +1,20 @@
 package me.choco.locksecurity;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Set;
+import java.util.UUID;
+
+import org.apache.commons.io.FileUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+import me.choco.locksecurity.api.LockedBlock;
+import me.choco.locksecurity.registration.PlayerRegistry;
+import me.choco.locksecurity.utils.LSPlayer;
+import me.choco.locksecurity.utils.general.ConfigAccessor;
+
 public final class TransferUtils {
 	
 	protected static final void fromDatabase(LockSecurity plugin) {
@@ -9,6 +24,50 @@ public final class TransferUtils {
 	
 	protected static final void fromFile(LockSecurity plugin) {
 		plugin.getLogger().info("Commencing transfer process for Data Support of LockSecurity 1.0.0 - 1.6.3");
-		// TODO: Transfer data from "locked.yml" (ConfigurationFile)
+		
+		PlayerRegistry playerRegistry = plugin.getPlayerRegistry();
+		
+		ConfigAccessor lockedFile = new ConfigAccessor(plugin, "locked.yml");
+		if (!plugin.infoFile.exists()) {
+			try {
+				plugin.infoFile.createNewFile();
+				FileUtils.write(plugin.infoFile, 
+						"nextLockID=" + lockedFile.getConfig().getInt("NextLockID")
+						+ "\nnextKeyID=" + lockedFile.getConfig().getInt("NextKeyID"), 
+					Charset.defaultCharset());
+			} catch (IOException e) {
+				plugin.getLogger().info("Could not load key/lock ID to file");
+				plugin.infoFile.delete();
+			}
+		}
+		
+		Set<String> keys = lockedFile.getConfig().getKeys(false);
+		keys.remove("NextLockID"); keys.remove("NextKeyID");
+		
+		for (String key : keys) {
+			int lockID = Integer.parseInt(key);
+			int keyID = lockedFile.getConfig().getInt(key + ".KeyID");
+			UUID ownerUUID = UUID.fromString(lockedFile.getConfig().getString(key + ".OwnerUUID"));
+			
+			World world = Bukkit.getServer().getWorld(lockedFile.getConfig().getString(key + ".Location.World"));
+			double x = lockedFile.getConfig().getDouble(key + ".Location.X");
+			double y = lockedFile.getConfig().getDouble(key + ".Location.Y");
+			double z = lockedFile.getConfig().getDouble(key + ".Location.Z");
+			Location location = new Location(world, x, y, z);
+			
+			LSPlayer player = playerRegistry.registerPlayer(Bukkit.getOfflinePlayer(ownerUUID));
+			if (player == null) { // Player has never existed on the server
+				plugin.getLogger().warning("Missing player with UUID \"" + ownerUUID + "\". Ignoring...");
+				continue;
+			}
+			
+			LockedBlock block = new LockedBlock(player, location, lockID, keyID);
+			player.addBlockToOwnership(block);
+			// No need to register it. This is done on world load
+		}
+		
+		plugin.getLogger().info("Transfer process completed! You may now delete the \"locked.yml\", or (recommended) keep as a backup"
+				+ "in case anything had went awry during the transfer process (i.e. missing data).");
+		plugin.getLogger().info("Thank you for using LockSecurity " + plugin.getDescription().getVersion() + ". Enjoy!");
 	}
 }
