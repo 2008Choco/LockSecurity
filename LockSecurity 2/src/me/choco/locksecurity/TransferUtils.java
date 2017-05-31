607d65f469d0a22c5a663559b6392fad2ed4d69a
Parker Hawke <hawkeboyz2@hotmail.com>
2017-05-30 21:10:52 -0400

    Added a method body for TransferUtils#fromDatabase()

--- LockSecurity 2/src/me/choco/locksecurity/TransferUtils.java
+++ LockSecurity 2/src/me/choco/locksecurity/TransferUtils.java
@@ -2,6 +2,11 @@ package me.choco.locksecurity;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Set;
 import java.util.UUID;
 
@@ -19,7 +24,50 @@ public final class TransferUtils {
 	
 	protected static final void fromDatabase(LockSecurity plugin) {
 		plugin.getLogger().info("Commencing transfer process for Data Support of LockSecurity 1.7.0 - 1.8.2");
-		// TODO: Transfer data from "lockinfo.db" (SQLite)
+
+		PlayerRegistry playerRegistry = plugin.getPlayerRegistry();
+		
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection connection = DriverManager.getConnection("jdbc:sqlite:plugins/LockSecurity/lockinfo.db");
+			Statement retrievalStatement = connection.createStatement();
+			
+			ResultSet result = retrievalStatement.executeQuery("SELECT * FROM LockedBlocks");
+			if (result == null) return;
+			
+			while (result.next()) {
+				int lockID = result.getInt("LockID");
+				int keyID = result.getInt("KeyID");
+				UUID ownerUUID = UUID.fromString(result.getString("OwnerUUID"));
+				
+				World world = Bukkit.getWorld(result.getString("LocationWorld"));
+				int x = result.getInt("LocationX");
+				int y = result.getInt("LocationY");
+				int z = result.getInt("LocationZ");
+				Location location = new Location(world, x, y, z);
+				
+				LSPlayer player = playerRegistry.registerPlayer(Bukkit.getOfflinePlayer(ownerUUID));
+				if (player == null) { // Player has never existed on the server
+					plugin.getLogger().warning("Missing player with UUID \"" + ownerUUID + "\". Ignoring...");
+					continue;
+				}
+				
+				LockedBlock block = new LockedBlock(player, location, lockID, keyID);
+				player.addBlockToOwnership(block);
+				
+				plugin.getLogger().info("Loaded block at " + location.getWorld().getName() + " " 
+						+ location.getBlockX() + ", " + location.getBlockY() + ", " + location.getBlockZ()
+						+ " for user " + player.getPlayer().getName() + " with LockID " + lockID + " and KeyID " + keyID);
+			}
+			
+			result.close();
+			retrievalStatement.close();
+			connection.close();
+		} catch (ClassNotFoundException | SQLException e) { e.printStackTrace(); }
+		
+		plugin.getLogger().info("Transfer process completed! You may now delete the \"lockinfo.db\", or (recommended) keep as a backup"
+				+ "in case anything had went awry during the transfer process (i.e. missing data).");
+		plugin.getLogger().info("Thank you for using LockSecurity " + plugin.getDescription().getVersion() + ". Enjoy!");
 	}
 	
 	protected static final void fromFile(LockSecurity plugin) {
